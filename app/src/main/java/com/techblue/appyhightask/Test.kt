9d1c128570ed90727d26202b6092093f7d82f0e5
gopineelam1925 <gopi.neelam@voltuswave.com>
2020-07-16 20:16:35 +0530

    issue fix at problem1

--- app/src/main/java/com/techblue/appyhightask/Test.kt
+++ app/src/main/java/com/techblue/appyhightask/Test.kt
@@ -1,20 +1,23 @@
 package com.techblue.appyhightask
 
 fun main() {
-    val arr = intArrayOf(1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1)
+    val arr = intArrayOf(1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1)
     var maxLength = 0
+    var globalMax = 0
 
     if (arr.size < 10000) {
         for (index in arr.indices) {
             if (arr[index] == 1) {
                 maxLength += 1
             } else {
-                if (index < arr.size - 1)
+                if (index < arr.size - 1) {
+                    globalMax = maxLength
                     maxLength = 0
+                }
             }
         }
 
-        System.out.println("$maxLength")
+        System.out.println("$globalMax")
     } else {
         System.out.println("Length of the array exceeds 10000")
     }
